029e88a91f6b2506df8b2bc4b4ddf0628c86b226
RiyadHassen <riyadworku@gmail.com>
2021-01-30 10:56:03 +0300

    sort colors

---
+++ D6/sortcolor.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 10:53:45 2021
+
+@author: riyad
+"""
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
