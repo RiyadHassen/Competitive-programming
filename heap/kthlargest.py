c44876ea63fe5f781aa917a5adf2ed8a328c2f65
RiyadHassen <riyadworku@gmail.com>
2021-02-05 22:40:18 +0300

    kthlargest element

---
+++ heap/kthlargest.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 22:38:36 2021
+
+@author: riyad
+"""
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k 
+        self.nums = nums
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.nums.sort()
+        #h = []
+        # temp =len(self.nums)-self.k
+        # print(self.nums)
+        # for i in range(len(self.nums)-1,temp-1,-1):
+        #     heapq.heappush(h,-1 * self.nums[i])
+        # print(h)    
+        # for i in range(self.k-2):
+        #     heapq.heappop(h)
+        tmp = len(self.nums)-self.k
+        return self.nums[tmp]
+        
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
