ad2e14532019a99f10f74de76ee0dab705743de5
RiyadHassen <riyadworku@gmail.com>
2021-03-24 15:44:38 +0300

    update triangle sum

--- DynamicProg/triangleSum.py
+++ DynamicProg/triangleSum.py
@@ -1,13 +1,14 @@
 class Solution:
-    def largestSumAfterKNegations(self, A: List[int], K: int) -> int:
-        
-        for i in range(K):
-            min_num = float('inf')
-            min_index = float('inf')
-            for j in range(len(A)):
-                if A[j] < min_num:
-                    min_num = A[j]
-                    min_index = j
-            if A[min_index]!=0:
-                A[min_index] = -A[min_index]
-        return sum(A)
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        memo ={}
+        return self.min_path(triangle,0,0,memo)
+    def min_path(self,triangle,row,col,memo):
+        if row==len(triangle):
+            return 0
+        if (row,col) in memo:
+            return memo[(row,col)]
+        left = self.min_path(triangle,row+1,col,memo)
+        right = self.min_path(triangle,row+1,col+1,memo)
+        memo[(row,col)] = triangle[row][col]+min(left,right)
+        return memo[(row,col)]
+    
